45442fb405f44544e4c2abaf8c4eb2ac78081677
Artur Gottmann <artur.gottmann@kit.edu>
2022-05-03 18:37:50 +0200

    extend 1D limits script

--- create_1D_limits_yaml.py
+++ create_1D_limits_yaml.py
@@ -18,7 +18,7 @@ parser.add_argument(
 parser.add_argument(
     "--process",
     required=True,
-    help="Long and short name of the process, separated with : in the string",
+    help="Short name of the process in Latex",
 )
 parser.add_argument(
     "--type-string", required=True, help="Particular type of the limit result presented"
@@ -27,39 +27,46 @@ parser.add_argument(
     "--output-file", required=True, help="Basename of the output .yaml file"
 )
 parser.add_argument(
+    "--mass-quantity", required=True, help="Label:units of the mass quantity"
+)
+parser.add_argument(
+    "--limit-quantity", required=True, help="Label:units of the limit quantity"
+)
+parser.add_argument(
     "--output-directory",
     required=True,
     help="Main output directory name for submission to HEPData",
 )
+parser.add_argument(
+    "--additional-qualifiers", nargs="*", help="A list of additional qualifiers, given as name:value" # Like the process label (long version)
+)
 
 args = parser.parse_args()
+
+m_label, m_units = args.mass_quantity.split(":")
+l_label, l_units = args.limit_quantity.split(":")
 
 
 # Setting up templates
 limit_template_1D_main = {
     "dependent_variables": [],
-    "independent_variables": [
-        {
-            "header": {"name": r"Mass $m_\phi$", "units": "GeV"},
-            "values": [],
-        }
-    ],
+    "independent_variables": [],
 }
 
 limit_template_1D_individual = {
-    "header": {
-        "name": r"95% CL limit on $\sigma(PROCESS)B(\phi\rightarrow\tau\tau)$",
-        "units": "pb",
-    },
+    "header": {},
     "qualifiers": [
         {"name": r"Centre-of-mass energy $\sqrt{s}$", "units": "GeV", "value": 13000},
         {"name": r"Integrated luminosity", "units": "fb$^{-1}$", "value": 138},
-        {"name": r"Higgs boson production", "value": "PROCESS_LABEL"},
         {"name": r"Limit", "value": "RESULT"},
         {"name": r"Type", "value": "TYPE"},
     ],
     "values": [],
 }
+
+for q in args.additional_qualifiers:
+    name,value = q.split(":")
+    limit_template_1D_individual["qualifiers"].append({"name": name, "value": value})
 
 limit_template_expvalues = {
     "value": 0.0,
@@ -82,28 +89,25 @@ masses = sorted([float(m) for m in results.keys()])
 
 # Setting up the .yaml file dict
 output = copy.deepcopy(limit_template_1D_main)
-label, short = args.process.split(":")
+short = args.process
 
 ## Put masses in
-mass_dict = None
-for v in output["independent_variables"]:
-    if "Mass" in v["header"]["name"]:
-        mass_dict = v
-        break
+mass = copy.deepcopy(limit_template_1D_individual)
+mass.pop("qualifiers")
+mass["header"]["name"] = m_label
+mass["header"]["units"] = m_units
 for m in masses:
-    mass_dict["values"].append({"value": m})
+    mass["values"].append({"value": m})
+output["independent_variables"].append(mass)
 
 ## Include observed
 obs = copy.deepcopy(limit_template_1D_individual)
-obs["header"]["name"] = obs["header"]["name"].replace("PROCESS", short)
+obs["header"]["name"] = l_label
+obs["header"]["units"] = l_units
 
 ### Replace placeholders
 for q in obs["qualifiers"]:
-    if "production" in q["name"]:
-        q["value"] = q["value"].replace(
-            "PROCESS_LABEL", " ".join([label, "$" + short + "$"])
-        )
-    elif "Limit" in q["name"]:
+    if "Limit" in q["name"]:
         q["value"] = q["value"].replace("RESULT", "Observed")
     elif "Type" in q["name"]:
         q["value"] = q["value"].replace("TYPE", args.type_string)
@@ -115,15 +119,12 @@ output["dependent_variables"].append(obs)
 
 ## Include Expected +/-68 %
 exp68 = copy.deepcopy(limit_template_1D_individual)
-exp68["header"]["name"] = exp68["header"]["name"].replace("PROCESS", short)
+exp68["header"]["name"] = l_label
+exp68["header"]["units"] = l_units
 
 ### Replace placeholders
 for q in exp68["qualifiers"]:
-    if "production" in q["name"]:
-        q["value"] = q["value"].replace(
-            "PROCESS_LABEL", " ".join([label, "$" + short + "$"])
-        )
-    elif "Limit" in q["name"]:
+    if "Limit" in q["name"]:
         q["value"] = q["value"].replace("RESULT", "Expected $\pm68\%$")
     elif "Type" in q["name"]:
         q["value"] = q["value"].replace("TYPE", args.type_string)
@@ -144,15 +145,12 @@ output["dependent_variables"].append(exp68)
 
 ## Include Expected +/-95 %
 exp95 = copy.deepcopy(limit_template_1D_individual)
-exp95["header"]["name"] = exp95["header"]["name"].replace("PROCESS", short)
+exp95["header"]["name"] = l_label
+exp95["header"]["units"] = l_units
 
 ### Replace placeholders
 for q in exp95["qualifiers"]:
-    if "production" in q["name"]:
-        q["value"] = q["value"].replace(
-            "PROCESS_LABEL", " ".join([label, "$" + short + "$"])
-        )
-    elif "Limit" in q["name"]:
+    if "Limit" in q["name"]:
         q["value"] = q["value"].replace("RESULT", "Expected $\pm95\%$")
     elif "Type" in q["name"]:
         q["value"] = q["value"].replace("TYPE", args.type_string)
