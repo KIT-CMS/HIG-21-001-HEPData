fc33d094f084051d5429cb09f0cb4835fbf1f949
Artur Gottmann <artur.gottmann@kit.edu>
2022-06-16 16:16:41 +0200

    remove unnecessary functionalities

--- create_postfit_shapes_all_categories.py
+++ create_postfit_shapes_all_categories.py
@@ -3,10 +3,9 @@
 import asyncio
 import argparse
 import yaml
-import signal
-import subprocess
 
 command_template = "./create_postfit_distributions_yaml.py --input data/{INPUTFOLDER}/{CAT}_hepdata.root --analysis-configuration analysis_configuration_grouped.yaml --output-file {CAT}_hepdata_grouped_postfit_{OUTPOSTFIX}.yaml --distribution-quantity '{QUANTITY}' --category {CAT} --output-directory submission_preparation --min-bin-content 5e-4 --signal-pattern '(.*)_(\d*)' --mode grouped {ADDITIONAL}"
+
 
 async def execute_command(queue, name):
     interrupted = False
@@ -24,8 +23,8 @@ async def execute_command(queue, name):
             try:
                 sub_process = await asyncio.create_subprocess_shell(
                     task,
-                    stdout = asyncio.subprocess.PIPE,
-                    stderr = asyncio.subprocess.PIPE,
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE,
                 )
                 stdout, stderr = await sub_process.communicate()
             except asyncio.CancelledError:
@@ -38,12 +37,15 @@ async def execute_command(queue, name):
             continue
     return
 
+
 async def main(n_threads, commands):
     creation_task_queue = asyncio.Queue()
 
     create_worker = []
     for i in range(n_threads):
-        task = asyncio.ensure_future(execute_command(creation_task_queue, f"create_worker_{i}"))
+        task = asyncio.ensure_future(
+            execute_command(creation_task_queue, f"create_worker_{i}")
+        )
         create_worker.append(task)
 
     for command in commands:
@@ -59,10 +61,20 @@ async def main(n_threads, commands):
             task.cancel()
         await asyncio.gather(*create_worker, return_exceptions=True)
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Script to create postfit shapes for HEPData queue-based")
-    parser.add_argument("--n-threads", required=True, type=int, help="Number of threads to be used")
-    parser.add_argument("--categories-configurations", required=True, nargs="+", help="Number of threads to be used")
+    parser = argparse.ArgumentParser(
+        description="Script to create postfit shapes for HEPData queue-based"
+    )
+    parser.add_argument(
+        "--n-threads", required=True, type=int, help="Number of threads to be used"
+    )
+    parser.add_argument(
+        "--categories-configurations",
+        required=True,
+        nargs="+",
+        help="Number of threads to be used",
+    )
 
     args = parser.parse_args()
     commands = []
@@ -71,24 +83,24 @@ if __name__ == "__main__":
         for c in conf["catlist"]:
             additional = ""
             if conf["additional_inputs"] and conf["additional_signals"]:
-                additional = " ".join(["--additional-inputs", conf["additional_inputs"].format(CAT=c), "--additional-signals", "'" + conf["additional_signals"] + "'"])
+                additional = " ".join(
+                    [
+                        "--additional-inputs",
+                        conf["additional_inputs"].format(CAT=c),
+                        "--additional-signals",
+                        "'" + conf["additional_signals"] + "'",
+                    ]
+                )
             command = command_template.format(
                 CAT=c,
                 QUANTITY=conf["quantity"],
                 INPUTFOLDER=conf["input_folder"],
                 OUTPOSTFIX=conf["output_file_postfix"],
-                ADDITIONAL=additional).strip()
+                ADDITIONAL=additional,
+            ).strip()
             commands.append(command)
 
-    loop = asyncio.get_event_loop()
-    main_task = asyncio.ensure_future(main(args.n_threads, commands))
-    signals = [signal.SIGHUP, signal.SIGTERM, signal.SIGINT]
-    for s in signals:
-        loop.add_signal_handler(s, main_task.cancel)
-
     try:
-        loop.run_until_complete(main_task)
+        asyncio.run(main(args.n_threads, commands))
     except asyncio.CancelledError:
         pass
-    finally:
-        loop.close()
